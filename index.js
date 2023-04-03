8424cb8ae3257954013dc4d57547e6367e98eb72
Sylvia Mumbi <mumbi099@gmail.com>
2023-04-03 14:37:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+//Task1
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+
+//Task2
+function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home."));
+
+//Task3
+function wrapAdjective(symbol ="*"){
+    return function(adj){
+        return (`You are ${symbol}${adj}${symbol}!`)
+    }
+}
